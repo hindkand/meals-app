111cb02677daa2797cf86a6cb883e3c6989884c0
hindkand <108768182+hindkand@users.noreply.github.com>
2023-03-20 23:19:09 +0200

    first commit

---
+++ lib/screens/tabs_screens.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+import '../widgets/main_drawer.dart';
+import 'favorites_screen.dart';
+import 'categories_screen.dart';
+import '../models/mael.dart';
+
+class TabsScreen extends StatefulWidget {
+  final List<Meal> favoriteMeals;
+
+  TabsScreen(this.favoriteMeals);
+
+  @override
+  State<TabsScreen> createState() => _TabsScreenState();
+}
+
+class _TabsScreenState extends State<TabsScreen> {
+  late List<Map<String, Object>> _pages;
+  int _selectedPageIndex = 0;
+
+  @override
+  void initState() {
+    _pages = [
+      {
+        'page': CategoriesScreen(),
+        'title': 'Categories',
+      },
+      {
+        'page': FavoritesScreen(widget.favoriteMeals),
+        'title': 'Your Favorite',
+      },
+    ];
+    super.initState();
+  }
+
+  void _selectPage(int index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_pages[_selectedPageIndex]['title'] as String),
+      ),
+      drawer: MainDrawer(),
+      body: _pages[_selectedPageIndex]['page'] as Widget,
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _selectPage,
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        unselectedItemColor: Colors.white,
+        selectedItemColor: Theme.of(context).colorScheme.secondary,
+        currentIndex: _selectedPageIndex,
+        // type: BottomNavigationBarType.fixed,
+        items: [
+          BottomNavigationBarItem(
+            backgroundColor: Theme.of(context).colorScheme.primary,
+            icon: Icon(Icons.category),
+            label: 'Categories',
+          ),
+          BottomNavigationBarItem(
+            backgroundColor: Theme.of(context).colorScheme.primary,
+            icon: Icon(Icons.star),
+            label: 'Favorites',
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
+/*
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      // initialIndex: 0,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Meals'),
+          bottom: TabBar(
+            tabs: <Widget>[
+              Tab(
+                icon: Icon(
+                  Icons.category,
+                ),
+                text: 'Categories',
+              ),
+              Tab(
+                icon: Icon(
+                  Icons.star,
+                ),
+                text: 'Favorites',
+              ),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: <Widget>[
+            CategoriesScreen(),
+            FavoritesScreen(),
+          ],
+        ),
+      ),
+    );
+  }
+*/
